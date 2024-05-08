71525f5093086f2d106b7fb575c922509d379a65
calvincho95 <calvincho95@naver.com>
2024-05-08 14:00:27 +0900

    5th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Header from "./components/Header";
+import CardList from "./components/CardList";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex flex-col">
-      <header />
-      <section className=" bg-green-100 grow px-2">
+      <Header />
+      <Section className=" bg-green-100 grow px-2">
         <CardList />
-      </section>
+      </Section>
       <Footer />
     </div>
   );
